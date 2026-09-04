4a5cff9412669ba964f781f0a67e09065661ccb5
u24697274 <u24697274@tuks.co.za>
2026-09-04 11:31:32 +0200

    Added base state class declaration

--- State.h
+++ State.h
@@ -1,4 +1,21 @@
 #ifndef STATE_H
 #define STATE_H
 
+#include <iostream>
+#include <string>
+
+#include "DeliveryComponent.h"
+
+class State
+{
+    public:
+        State(){} //empty Constructor
+        virtual bool handleChange(DeliveryComponent* d) = 0; //PURE VIRTUAL allows to change states
+        std::string getState(){return this->name;}; //Return what the name of the state
+        virtual ~State(){} //virtual destructor
+
+    private:
+        std::string name; //name of the state
+};
+
 #endif //STATE_H
